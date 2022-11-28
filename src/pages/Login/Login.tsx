8a39908c4001bec64c4faf1909212e3471b97bba
Maharani Ayu Putri Irawan <72875641+rannnayy@users.noreply.github.com>
2022-11-28 16:42:48 +0700

    Fix size of input in login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -60,39 +60,41 @@ const Login = () => {
                 <img className="headerImg" src="/Spotify_Logo_RGB_Black.png"/>
                 <hr/>
             </div>
-            <div className="loginPrompt">To continue, log in to Spotify.</div>
-            {
-                login ? 
-                    <div className="loginFail">
-                        <img src="/icons8-warning-67.png" />
-                        <div className="incorrectUnamePass">Incorrect username or password.</div>
-                    </div> 
-                :   <></>
-            }
-            <a className="loginPhone link" href="">CONTINUE WITH PHONE NUMBER</a>
-            <div className="loginOr">
-                <hr className='loginOrHr'/>
-                <div className="loginOrText">OR</div>
-                <hr className='loginOrHr'/>
+            <div className="login">
+                <div className="loginPrompt">To continue, log in to Spotify.</div>
+                {
+                    login ? 
+                        <div className="loginFail">
+                            <img src="/icons8-warning-67.png" />
+                            <div className="incorrectUnamePass">Incorrect username or password.</div>
+                        </div> 
+                    :   <></>
+                }
+                <a className="loginPhone link" href="">CONTINUE WITH PHONE NUMBER</a>
+                <div className="loginOr">
+                    <hr className='loginOrHr'/>
+                    <div className="loginOrText">OR</div>
+                    <hr className='loginOrHr'/>
+                </div>
+                <div className="loginFormCt">
+                    <form method="POST" className="form">
+                        <label className="loginFormLabel" htmlFor='emailid'>Email address or username</label><br />
+                        <input className="loginFormInput" type="text" name="emailid" placeholder="Email address or username" onChange={(e) => setEmail(e.target.value)} /><br />
+                        <label className="loginFormLabel" htmlFor="password">Password</label><br />
+                        <input className="loginFormInput" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+                        <div className="loginFormForgot" onClick={() => {setOpenModal(true)}}>Forgot your password?</div>
+                        <br /><br />
+                        <input className="loginFormBt" type="button" onClick={() => checkLogin()} value="LOG IN" />
+                    </form>
+                </div>
+                <Modal isOpen={isOpenModal} onRequestClose={closeModal} className="mymodal" overlayClassName="myoverlay">
+                    <div className='modalText'>Try to remember it next time!</div>
+                </Modal>
+                <hr className='hrbawahbutton'/>
+                <div className="loginSignupText">Don't have an account?</div>
+                <Link className="loginSignupBt link" to="/register">SIGN UP FOR SPOTIFY</Link>
+                <div id="redirect"></div>
             </div>
-            <div className="loginFormCt">
-                <form method="POST">
-                    <label className="loginFormLabel" htmlFor='emailid'>Email address or username</label><br />
-                    <input className="loginFormInput" type="text" name="emailid" placeholder="Email address or username" onChange={(e) => setEmail(e.target.value)} /><br />
-                    <label className="loginFormLabel" htmlFor="password">Password</label><br />
-                    <input className="loginFormInput" type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
-                    <div className="loginFormForgot" onClick={() => {setOpenModal(true)}}>Forgot your password?</div>
-                    <br /><br />
-                    <input className="loginFormBt" type="button" onClick={() => checkLogin()} value="LOG IN" />
-                </form>
-            </div>
-            <Modal isOpen={isOpenModal} onRequestClose={closeModal} className="mymodal" overlayClassName="myoverlay">
-                <div className='modalText'>Try to remember it next time!</div>
-            </Modal>
-            <hr className='hrbawahbutton'/>
-            <div className="loginSignupText">Don't have an account?</div>
-            <Link className="loginSignupBt link" to="/register">SIGN UP FOR SPOTIFY</Link>
-            <div id="redirect"></div>
         </div>
     );
 };
